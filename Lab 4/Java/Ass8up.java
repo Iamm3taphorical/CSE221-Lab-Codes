e2f5ece6ec0fb40c084cc4abf5e039b02e0bd007
Mahir Dyan <mahirdyan30@gmail.com>
2026-01-01 20:27:51 +0600

    Add Lab 4 Ass8up (Java)

---
+++ Lab 4/Java/Ass8up.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Ass8up {
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt())
+            return;
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int q = sc.nextInt();
+
+            List<List<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i <= n; i++)
+                adj.add(new ArrayList<>());
+
+            for (int i = 1; i <= n; i++) {
+                // Should check if logic is identical.
+                // Ass3up: iterate 1..n, 1..n. if coprime add to list.
+                // Ass8up: same.
+                for (int j = 1; j <= n; j++) {
+                    if (i != j && gcd(i, j) == 1) {
+                        adj.get(i).add(j);
+                    }
+                }
+            }
+
+            while (q-- > 0) {
+                int u = sc.nextInt();
+                int k = sc.nextInt();
+                if (k > adj.get(u).size()) {
+                    System.out.println("-1");
+                } else {
+                    System.out.println(adj.get(u).get(k - 1));
+                }
+            }
+        }
+    }
+}
