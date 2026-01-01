6f06bbf255d330fef4299afaee4e3fb1e6924a89
Mahir Dyan <mahirdyan30@gmail.com>
2026-01-01 20:20:35 +0600

    Add Lab 2 Problem D1 (Java)

---
+++ Lab 2/Java/ProblemD1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class ProblemD1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt())
+            return;
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        int m = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++)
+            b[i] = sc.nextInt();
+
+        int l = 0, r = 0;
+        StringBuilder sb = new StringBuilder();
+        while (l < n && r < m) {
+            if (a[l] <= b[r]) {
+                sb.append(a[l]).append(" ");
+                l++;
+            } else {
+                sb.append(b[r]).append(" ");
+                r++;
+            }
+        }
+        while (l < n) {
+            sb.append(a[l]).append(" ");
+            l++;
+        }
+        while (r < m) {
+            sb.append(b[r]).append(" ");
+            r++;
+        }
+        System.out.println(sb.toString().trim());
+    }
+}
