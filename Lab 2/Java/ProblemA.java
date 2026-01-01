fedd64dc1c1996d2d9636290566435f233c2921c
Mahir Dyan <mahirdyan30@gmail.com>
2026-01-01 20:20:35 +0600

    Add Lab 2 Problem A (Java)

---
+++ Lab 2/Java/ProblemA.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ProblemA {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt())
+            return;
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int left = 0;
+        int right = n - 1;
+        boolean found = false;
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == s) {
+                System.out.println((left + 1) + " " + (right + 1));
+                found = true;
+                break;
+            } else if (sum < s) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        if (!found) {
+            System.out.println("-1");
+        }
+    }
+}
