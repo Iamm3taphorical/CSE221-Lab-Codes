5d5e1a55b84fa9911b7541e27cc909ea83f3cda7
Mahir Dyan <mahirdyan30@gmail.com>
2026-01-01 20:20:35 +0600

    Add Lab 2 Problem B1 (Java)

---
+++ Lab 2/Java/ProblemB1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ProblemB1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt())
+            return;
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++)
+            b[i] = sc.nextInt();
+
+        int left = 0;
+        int right = m - 1;
+        int bestI = 0;
+        int bestJ = 0;
+        int bestDiff = Integer.MAX_VALUE;
+
+        while (left < n && right >= 0) {
+            int sum = a[left] + b[right];
+            int diff = Math.abs(sum - k);
+            if (diff < bestDiff) {
+                bestDiff = diff;
+                bestI = left;
+                bestJ = right;
+            }
+            if (sum < k) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        System.out.println((bestI + 1) + " " + (bestJ + 1));
+    }
+}
